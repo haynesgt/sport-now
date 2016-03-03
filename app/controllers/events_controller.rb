4c674ed3973cd05bbe0e027b7b3108b63c382fd5
Gavin Haynes <haynes.gt@gmail.com>
2016-03-03 21:37:52 +0000

    Fix events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -69,6 +69,6 @@ class EventsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def event_params
-      params.require(:event).permit(:owner, :sport, :lat, :long, :location)
+      params.require(:event).permit(:owner_uid, :sport, :lat, :long, :location)
     end
 end
